f40f661693df656a5c5ad8cf87da5a5fcfd18988
R1kaB3rN <100738684+R1kaB3rN@users.noreply.github.com>
2024-10-10 11:37:36 -0700

    umu_runtime: generate runtime digest on update or install

--- umu/umu_runtime.py
+++ umu/umu_runtime.py
@@ -188,7 +188,19 @@ def _install_umu(
     UMU_LOCAL.joinpath("_v2-entry-point").rename(UMU_LOCAL.joinpath("umu"))
 
     # Validate the runtime after moving the files
-    check_runtime(UMU_LOCAL, json)
+    ret = check_runtime(UMU_LOCAL, json)
+
+    # Compute a digest of the metadata for future attestation
+    if ret != 1:
+        # At this point, the runtime is authenticated. For subsequent launches,
+        # we'll check against our digest to ensure we're intact
+        digest_ret = get_runtime_digest(UMU_LOCAL)
+        UMU_LOCAL.joinpath("umu.hashsum").write_text(
+            digest_ret, encoding="utf-8"
+        )
+        return
+
+    log.warning("steamrt validation failed, skipping metadata checksum")
 
 
 def setup_umu(
